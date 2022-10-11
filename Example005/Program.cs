849da0756068113ec4529cb892b9b0f10d445331
vladen108 <vladen108@gmail.com>
2022-10-11 22:44:25 +0400

    Add Examle005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, it's Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
